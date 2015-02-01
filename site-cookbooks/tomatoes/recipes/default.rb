3ec63a3ed6f6efacd04869cf47c1de56398dabaf
Mark Yang <markyangzw@gmail.com>
2015-02-01 00:28:32 -0500

    First Commit

---
+++ site-cookbooks/tomatoes/recipes/default.rb
@@ -0,0 +1,23 @@
+include_recipe 'git'
+include_recipe 'nodejs'
+user 'random' do
+  supports :manage_home => true
+  home '/home/random'
+end
+
+nodejs_npm 'forever'
+
+git '/home/random/TODO' do
+  repository 'https://github.com/markyang/TODO_APP.git'
+  user 'random'
+end
+
+nodejs_npm '' do
+  path '/home/random/TODO'
+  user 'random'
+end
+
+execute 'forever server.js' do
+  cwd '/home/random/TODO'
+  user 'random'
+end
